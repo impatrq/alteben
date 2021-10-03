1051ed727077350953597052b097efa717ebefe3
matiasordoniez <80414082+matiasordoniez@users.noreply.github.com>
2021-10-03 13:19:13 -0300

    Programar sensor con ATMEGA

--- main.c
+++ main.c
@@ -17,17 +17,11 @@ int sensor = 0;
 double Vs = 5;
 double presion_sensor;
 double Vout;
-<<<<<<< HEAD:main.c
 double presion_final;
 double altura;
 char alturaF[8];
 char presion_finalF[8];
-=======
-float presion_final;
-float altura;
-char alturaF;
-char presion_finalF;
->>>>>>> a9726fee2b01ea83c3015c45241f0bd94903119c:main1.c
+
 
 //---------------------------------------------------------------PROGRAMA-----------------------------------------------------------------------
 
@@ -65,17 +59,11 @@ int main(void)
 	PORTC = 0b00000001;
 	
 	
-	
-	
-	
 	//iniciamos LCD
-<<<<<<< HEAD:main.c
+
     LCD_Init();
 	
-=======
-    lcd_init(LCD_DISP_ON);
-	lcd_clrscr();
->>>>>>> a9726fee2b01ea83c3015c45241f0bd94903119c:main1.c
+
     while (1) 
     {
 		//Seleccion del canal
@@ -93,10 +81,9 @@ int main(void)
 		presion_final= presion_atmosferica - presion_sensor;
 		//Definicion de altura
 		altura = -7999*log(presion_final);
-		//Pasaje de Float a String
-<<<<<<< HEAD:main.c
-		sprintf(presion_finalF, "%0.2f", presion_final);
-		sprintf(alturaF, "%0.2f", altura);
+		
+
+		
 		//Muestreo de valores en LCD
 		
 		LCD_Clear();
@@ -109,26 +96,6 @@ int main(void)
 		LCD_WriteFloat(altura);
 		*/
 		_delay_ms(500);
-=======
-		sprintf(presion_final, "%0.2f", presion_finalF);
-		sprintf(altura, "%0.2f", alturaF);
-		//Muestreo de valores en LCD
-		lcd_gotoxy(0,0);
-		lcd_puts("Presion= ");
-		
-		lcd_gotoxy(9,0);
-		lcd_puts("presion_finalF");
-		
-		
-		lcd_gotoxy(12,0);
-		lcd_puts("kPa");
-		
-		lcd_gotoxy(0,5);
-		lcd_puts("Altura= ");
-		
-		lcd_gotoxy(9,5);
-		lcd_puts("alturaF");
-		
->>>>>>> a9726fee2b01ea83c3015c45241f0bd94903119c:main1.c
+
     }
 }
