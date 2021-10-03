6a4274d30a1f426d7e7f30b66b443d5dcbca3169
matiasordoniez <80414082+matiasordoniez@users.noreply.github.com>
2021-10-03 12:55:51 -0300

    programar sensor con ATMEGA

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
  * Created: 30/06/2021 13:54:53
  * Author : Matias Ordo�ez
  */ 
-#define F_CPU 4000000UL
 #include <avr/io.h>
 #include "lcd.h"
-#include "adc_atmega_328.h"
 #include <stdlib.h>
 #include <util/delay.h>
 #include <math.h>
+#include <stdio.h>
 
+//definicion de variables
 double presion_atmosferica = 1013.25;
 int sensor = 0;
 double Vs = 5;
@@ -19,7 +19,12 @@ double presion_sensor;
 double Vout;
 double presion_final;
 double altura;
+char alturaF[8];
+char presion_finalF[8];
 
+//---------------------------------------------------------------PROGRAMA-----------------------------------------------------------------------
+
+//Configuracion del ADC
 void ADC_Configuracion()
 {
 	ADMUX = (1<<REFS0);
@@ -37,15 +42,16 @@ unsigned int ADC_Lectura(int canal){
 	return (ADC);
 }
 
+//---------------------------------------------------------------MAIN---------------------------------------------------------------------------
 
-
+//Funcion principal
 int main(void)
 {
 	int ADC_val;
 	int canal_seleccionado = 0;
 	ADC_Configuracion();
 	
-	
+	//Puertos de conexion
 	DDRD = 0b00000000;
 	PORTD = 0b00000000;
 	DDRC = 0b00000001;
@@ -55,43 +61,41 @@ int main(void)
 	
 	
 	
+	//iniciamos LCD
+    LCD_Init();
 	
-    lcd_init(LCD_DISP_ON);
-	lcd_clrscr();
     while (1) 
     {
+		//Seleccion del canal
 		ADC_val = ADC_Lectura(canal_seleccionado);
 		PORTC = ADC_val;
+		//Pasaje de ADC a un valor en Voltaje
 		int aux=0;
 		for(int i=0;i<10;i++){
 			aux=aux + (canal_seleccionado * 5.0/1023.0);
 			_delay_ms(5);
 		}
 		Vout=aux/10.0;
-
+		//Calculos de presion 
 		presion_sensor= Vout/Vs/0.009 - 0.04;
 		presion_final= presion_atmosferica - presion_sensor;
-		
+		//Definicion de altura
 		altura = -7999*log(presion_final);
+		//Pasaje de Float a String
+		sprintf(presion_finalF, "%0.2f", presion_final);
+		sprintf(alturaF, "%0.2f", altura);
+		//Muestreo de valores en LCD
 		
-		
-		
-	
-		lcd_gotoxy(0,0);
-		lcd_puts("Presion= ");
-		
-		lcd_gotoxy(9,0);
-		lcd_data(presion_final);
-		
-		lcd_gotoxy(12,0);
-		lcd_puts("kPa");
-		
-		lcd_gotoxy(0,5);
-		lcd_puts("Altura= ");
-		
-		lcd_gotoxy(9,5);
-		lcd_data(altura);
-		
+		LCD_Clear();
+		LCD_WriteString("Presion= ");
+		LCD_WriteFloat(presion_final);
+		LCD_WriteString("kPa");
+		/*
+		LCD_Set_Cursor(0, 1);
+		LCD_WriteString("Altura= ");
+		LCD_WriteFloat(altura);
+		*/
+		_delay_ms(500);
     }
 }
 
